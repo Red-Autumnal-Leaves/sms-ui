c623ecdb041bcf1436c46c72f3abf4be543e0212
nfw <nfw675660965@qq.com>
2018-01-15 10:08:53 +0800

    归档更新

---
+++ src/assets/js/libs/rem.js
@@ -0,0 +1,15 @@
+/**
+ * Created by admin on 2017/12/21.
+ */
+(function (doc, win, undefined) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in win? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (clientWidth === undefined) return;
+            docEl.style.fontSize = 100 * (clientWidth / 1080) + 'px';
+        };
+    if (doc.addEventListener === undefined) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
